ee122eeae686cdb7b89840807be014abe16e7fbe
Emiliano CV <cruddystingray1@gmail.com>
2025-11-11 12:49:18 -0600

    Add files via upload

---
+++ appCocoa1/appCocoa1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  appCocoa1
+//
+//  Created by Emiliano Cepeda on 13/09/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
